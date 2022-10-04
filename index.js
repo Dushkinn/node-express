4c1cf9ac423e74d39410e342a1b7b5adffa29979
Dushkinn <45728780+Dushkinn@users.noreply.github.com>
2022-10-04 16:56:32 +0600

    install handlebars, init layout for page rendering

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require("path");
+const expressHandlebars = require('express-handlebars');
+
 const app = express();
 
+const handlebars = expressHandlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.get('/', (req, res) => {
-    res.status(200);
-    res.sendfile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index');
 })
 
-app.get('/about', (req, res)=>{
-    res.sendfile(path.join(__dirname, 'views', 'about.html'));
+app.get('/about', (req, res) => {
+    res.render('about');
 })
 
 const PORT = process.env.PORT || 3000;
