a7c0ab044dd8d558d44247b73738049213430622
Dushkinn <45728780+Dushkinn@users.noreply.github.com>
2022-10-19 14:12:43 +0600

    update for work with mongo

--- routes/add.js
+++ routes/add.js
@@ -1,18 +1,20 @@
-const {Router} = require('express');
-const router = Router();
-const Course = require('../models/course');
+const {Router} = require('express')
+const Course = require('../models/course')
+const router = Router()
 
-router.get('/', (req, res)=>{
-    res.render('add',{
-        title: 'Add course',
-        isAdd: true,
-    });
-})
-router.post('/', (req, res)=>{
-    console.log(req.body)
-    const course = new Course(req.body.title, req.body.price , req.body.img);
-    res.redirect('/course/list')
+router.get('/', (req, res) => {
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  })
 })
 
+router.post('/', async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.img)
+
+  await course.save()
+
+  res.redirect('/courses')
+})
 
 module.exports = router
